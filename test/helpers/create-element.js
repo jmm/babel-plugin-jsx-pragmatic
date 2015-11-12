a991531702717282291709e2a4eb280b6d4b0536
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-11-12 16:42:34 -0500

    Add accidentally uncommited file.

---
+++ test/helpers/create-element.js
@@ -0,0 +1,9 @@
+exports = module.exports = function () {
+  return exports.createElement.apply(this, arguments);
+}
+
+exports.createElement = createElement;
+
+function createElement (el, props) {
+}
+// createElement
